598ed5a70a50cbdb39520345a64b11081f115a38
Reyoshina <hahauliashekl@gmail.com>
2025-04-25 22:40:17 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Создаем переменные 
+let moon = document.getElementById('moon');
+let text = document.getElementById('text');
+let train = document.getElementById('train');
+let man = document.getElementById('man');
+let desert_moon = document.getElementById('desert-moon');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    moon.style.top = value * 1 + 'px';
+    text.style.top = 80 + value * -0.05 + '%';
+    train.style.left = value * 1 + 'px';
+    man.style.left = value * 0.6 + 'px';
+    desert_moon.style.top = value * 0.3 + 'px';
+})
